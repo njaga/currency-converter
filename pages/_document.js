b6097e8ca6cd24e0f22d270d775d4cbea02d3250
Ndiaga Ndiaye <njaga01@outlook.fr>
2026-08-08 13:23:27 +0000

    Align browser chrome with Kiwango identity

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,12 @@ export default function Document() {
         <link rel="shortcut icon" href="/favicon.svg" />
         <link rel="apple-touch-icon" href="/favicon.svg" />
         <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content="#0F172A" />
-        <meta name="msapplication-TileColor" content="#0F172A" />
+        <meta name="theme-color" content="#059669" />
+        <meta name="msapplication-TileColor" content="#059669" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Kiwango" />
       </Head>
       <body className="antialiased">
         <Main />
