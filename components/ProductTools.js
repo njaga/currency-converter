986f2d7caa03a5d1c46ebf99e4d327fb0a449225
Ndiaga Ndiaye <njaga01@outlook.fr>
2026-08-09 17:21:33 +0000

    ux: keep the tools page focused on active tasks

--- components/ProductTools.js
+++ components/ProductTools.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ProductToolsCore from './ProductToolsCore';
-import KiwangoInsights from './KiwangoInsights';
 
 export default function ProductTools(props) {
-  return <><ProductToolsCore {...props} /><KiwangoInsights allRates={props.allRates} currencies={props.currencies} lang={props.lang} /></>;
+  return <ProductToolsCore {...props} />;
 }
